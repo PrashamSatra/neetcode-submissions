b9b00e4082bedd0f85c175adea4d9089cc0f57ed
PrashamSatra <78354709+PrashamSatra@users.noreply.github.com>
2026-09-03 00:07:43 +0530

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> bits(n + 1, 0);
+
+        int power = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == power * 2) {
+                power *= 2;
+            }
+
+            bits[i] = bits[i - power] + 1;
+        }
+
+        return bits;
+    }
+};
